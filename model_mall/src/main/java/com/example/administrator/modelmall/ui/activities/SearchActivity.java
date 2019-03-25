d5d694a68f9470710eafac14f94b31373120a038
Administrator <1246771571@qq.com>
2019-03-25 18:54:10 +0800

    搜索框开发

--- model_mall/src/main/java/com/example/administrator/modelmall/ui/activities/SearchActivity.java
+++ model_mall/src/main/java/com/example/administrator/modelmall/ui/activities/SearchActivity.java
@@ -1,21 +1,49 @@
 package com.example.administrator.modelmall.ui.activities;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.administrator.modelmall.R;
+import com.example.administrator.modelmall.ui.customview.ToastUtils;
+
+import butterknife.BindView;
+import scut.carson_ho.searchview.ICallBack;
+import scut.carson_ho.searchview.SearchView;
+import scut.carson_ho.searchview.bCallBack;
 
 public class SearchActivity extends BaseActivity {
-
+    @BindView(R.id.search_view)
+     SearchView searchView;
 
     @Override
     public Object offerLayout() {
         return R.layout.activity_search;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onBindView() {
+      setImmersionStatusBar();
 
+       initSearchView();
+    }
+
+    private void initSearchView() {
+
+        searchView.setOnClickSearch(new ICallBack() {
+            @Override
+            public void SearchAciton(String string) {
+                ToastUtils.showToast(SearchActivity.this,"后台小哥的数据库正在搭建",ToastUtils.LENGTH_LONG);
+            }
+        });
+        searchView.setOnClickBack(new bCallBack() {
+            @Override
+            public void BackAciton() {
+                finish();
+            }
+        });
     }
 
     @Override
